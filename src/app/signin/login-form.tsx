5d5f57bd58bd20b7769da923bcc2c9e201962927
Adeel <adeelasghar9292@gmail.com>
2024-03-17 01:17:48 +0500

    feat: create login form and validation schema

--- src/app/signin/login-form.tsx
+++ src/app/signin/login-form.tsx
@@ -1,5 +1,15 @@
 "use client";
 
+import FormInput from "@/components/FormInput";
+import { FormProvider } from "react-hook-form";
+
 export default function LoginForm() {
-  return <h2>Login Form</h2>;
+  return (
+    <form className="max-w-md w-full mx-auto overflow-hidden shadow-lg bg-ct-dark-200 rounded-2xl p-8 space-y-5">
+      <FormInput label="Email" name="email" type="email" />
+      <FormInput label="Password" name="password" type="password" />
+
+      <button>Login</button>
+    </form>
+  );
 }
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,18 +1,11 @@
+import LoginForm from "./login-form";
+
 export default function SignIn() {
   return (
     <div>
       <h1>Sign In</h1>
-      <form>
-        <label>
-          Email:
-          <input type="email" name="email" required />
-        </label>
-        <label>
-          Password:
-          <input type="password" name="password" required />
-        </label>
-        <button type="submit">Sign In</button>
-      </form>
+
+      <LoginForm />
     </div>
   );
 }
